1b52be1149ea5948d9c338d11aab8665cc3c1651
PavanKumarJi <100858300+PavanKumarJi@users.noreply.github.com>
2022-05-12 17:34:05 +0530

    Create Classes.js

---
+++ src/component/Classes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Classes extends Component {
+  render() {
+    return (
+        <div id="classes">
+          <h3 style={{marginTop:"1vw",marginBottom:"1vw"}}>made by using class component.</h3>
+          <p>external css applied</p>
+          <p style={{color:"white"}}>internal css applied</p>
+          </div>
+    )
+  }
+}
