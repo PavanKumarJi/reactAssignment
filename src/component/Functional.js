ee20e14991d09972c9786b80dd0f1b66913b41ac
PavanKumarJi <100858300+PavanKumarJi@users.noreply.github.com>
2022-05-12 17:33:22 +0530

    Create Functional.js

---
+++ src/component/Functional.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Functional = () => {
+  return (
+     <div id="functional">
+          <h3 style={{marginTop:"1vw",marginBottom:"1vw"}}>made by functional component</h3>
+          <p>external css applied</p>
+          <p style={{color:"red"}}>internal css applied</p>
+     </div>
+  )
+}
+export default Functional
